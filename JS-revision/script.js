f26b4deabda9306f77608a175e5b08672d486357
AshwaniMaurya2002 <ashwanikumarmaurya2002@gmail.com>
2026-04-30 18:36:37 +0530

    pushed

---
+++ JS-revision/script.js
@@ -0,0 +1,22 @@
+//? Prerequisites of react JS.
+
+//! arrow function
+//! rest and spread
+//! desturturing
+//! array method
+//! module
+//!  template literal
+
+// import { greet, demo } from "./utility.js";
+
+// greet();
+// demo();
+
+// import * as utility from "./utility.js";
+// utility.demo();
+// utility.greet;
+
+// import { greet as gt, demo as d } from "./utility";
+
+// gt();
+// d();
---
+++ JS-revision/utility.js
@@ -0,0 +1,11 @@
+//! Named funciton
+
+function greet() {
+  console.log("i am greet");
+}
+
+function demo() {
+  console.log(" i am demo");
+}
+
+export { greet, demo };
