0514e87f4010057aba38066b5fe198ea38ec6772
AshwaniMaurya2002 <ashwanikumarmaurya2002@gmail.com>
2026-05-21 18:46:36 +0530

    .

---
+++ React/src/12_LifeCycle/LifeCycleInCBC.jsx
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+
+export default class LifeCycleInCBC extends Component {
+  constructor() {
+    super();
+    console.log("I am Constructor");
+    this.state = { count: 8345365982359 };
+  }
+
+  increment = () => this.setState({ count: this.state.count + 1 });
+
+  componentDidMount() {
+    console.log("I am componentDidMount");
+    this.intervalId = setInterval(() => {
+      console.log("API CALLED");
+    }, 2000);
+  }
+
+  componentDidUpdate() {
+    console.log("Component Updated");
+  }
+
+  componentWillUnmount() {
+    console.log("Component is going to unmount");
+    clearInterval(this.intervalId);
+  }
+  render() {
+    console.log("I am Render");
+    return (
+      <div>
+        <h1>Count is {this.state.count}</h1>
+        <button onClick={this.increment}>Increment</button>
+      </div>
+    );
+  }
+}
--- React/src/App.jsx
+++ React/src/App.jsx
@@ -1,7 +1,7 @@
-import React, { use } from "react";
+import React, { use, useState } from "react";
 import InlineCss from "./09_ReactCss/InlineCss";
 // import Card from "./09_ReactCss/Card";
-import TodoWrapper from "./10 todoApp/TodoWrapper";
+import LifeCycleInCBC from "./12_LifeCycle/LifeCycleInCBC";
 // import ControlledForm2 from "./allTopics/08- Forms/ControlledForm2";
 // import Employee from "./07_list/Employee";
 // import UserList from "./07_list/UserList";
@@ -10,19 +10,16 @@ import TodoWrapper from "./10 todoApp/TodoWrapper";
 // import DrillingParent from "./04_props Drilling/DrillingParent";
 
 const App = () => {
+  const [toggle, setToggle] = useState(false);
+
+  const handleToggle = () => setToggle((prev) => !prev);
+
   return (
     <>
       {/* <DrillingParent /> */}
-      {/* <CallbackParent />
-       */}
-      {/* <UpliftingParent /> */}
-      {/* <Employee /> */}
-      {/* <UserList /> */}
-      {/* <ControlledForm2 /> */}
-      {/* <InlineCss /> */}
-      {/* <Ca
-      rd /> */}
-      <TodoWrapper />
+
+      <button onClick={handleToggle}>Toggle ME</button>
+      {toggle && <LifeCycleInCBC />}
     </>
   );
 };
