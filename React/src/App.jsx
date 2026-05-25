837cb13f716a1196ff656b4cea4c229e66255d35
AshwaniMaurya2002 <ashwanikumarmaurya2002@gmail.com>
2026-05-25 20:05:24 +0530

    .

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -3,6 +3,9 @@ import InlineCss from "./09_ReactCss/InlineCss";
 // import Card from "./09_ReactCss/Card";
 import LifeCycleInCBC from "./12_LifeCycle/LifeCycleInCBC";
 import LifeCycleInFBC from "./12_LifeCycle/LifeCycleInFBC";
+import Users from "./12_LifeCycle/Users";
+import Products from "./13_fetch/Products";
+import ProductTask from "./14_ProductTask/ProductTask";
 // import ControlledForm2 from "./allTopics/08- Forms/ControlledForm2";
 // import Employee from "./07_list/Employee";
 // import UserList from "./07_list/UserList";
@@ -11,9 +14,9 @@ import LifeCycleInFBC from "./12_LifeCycle/LifeCycleInFBC";
 // import DrillingParent from "./04_props Drilling/DrillingParent";
 
 const App = () => {
-  // const [toggle, setToggle] = useState(false);
+  const [toggle, setToggle] = useState(false);
 
-  // const handleToggle = () => setToggle((prev) => !prev);
+  const handleToggle = () => setToggle((prev) => !prev);
 
   return (
     <>
@@ -22,7 +25,14 @@ const App = () => {
       {/* <button onClick={handleToggle}>Toggle ME</button>
       {toggle && <LifeCycleInCBC />} */}
 
-      <LifeCycleInFBC />
+      {/* <LifeCycleInFBC /> */}
+      {/* <button onClick={handleToggle}>Toggle ME</button>
+      {toggle && <Users />} */}
+
+      {/* <Products
+       */}
+
+      <ProductTask />
     </>
   );
 };
